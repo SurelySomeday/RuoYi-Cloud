87b2a1bac79f7823e08e53af5b85f93dd526fc0b
yanxin <yanxin@autoai.com>
2023-04-23 14:52:26 +0800

    初步修改包名等信息

---
+++ learn-drive-api/learn-drive-api-system/src/main/java/com/aerozhonghuan/system/api/factory/RemoteLogFallbackFactory.java
@@ -0,0 +1,42 @@
+package com.aerozhonghuan.system.api.factory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.openfeign.FallbackFactory;
+import org.springframework.stereotype.Component;
+import com.aerozhonghuan.common.core.domain.R;
+import com.aerozhonghuan.system.api.RemoteLogService;
+import com.aerozhonghuan.system.api.domain.SysLogininfor;
+import com.aerozhonghuan.system.api.domain.SysOperLog;
+
+/**
+ * 日志服务降级处理
+ * 
+ * @author ruoyi
+ */
+@Component
+public class RemoteLogFallbackFactory implements FallbackFactory<RemoteLogService>
+{
+    private static final Logger log = LoggerFactory.getLogger(RemoteLogFallbackFactory.class);
+
+    @Override
+    public RemoteLogService create(Throwable throwable)
+    {
+        log.error("日志服务调用失败:{}", throwable.getMessage());
+        return new RemoteLogService()
+        {
+            @Override
+            public R<Boolean> saveLog(SysOperLog sysOperLog, String source)
+            {
+                return null;
+            }
+
+            @Override
+            public R<Boolean> saveLogininfor(SysLogininfor sysLogininfor, String source)
+            {
+                return null;
+            }
+        };
+
+    }
+}
